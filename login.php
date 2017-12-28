06900b57fb8c8d5959ebedf444c9d31839d68762
ludwing979 <ludwing979@github.com>
2017-12-27 18:02:10 -0600

    primer login

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Iniciar Sesión</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="titulo">
+		<img src="images/pizano.jpg">
+	</div>
+	<div id="fondo">
+		<div id="formulario">
+			<p>Ingresa tu correo</p>
+			<input type="email" id="correo">
+			<p>Contraseña</p>
+			<input type="password" id="pass"><br>
+			<input type="submit" value="Ingresar" onclick="">
+		</div>
+	</div>
+</body>
+</html>
