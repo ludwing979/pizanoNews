cc124839e1fb69baf9b4413aff0a8e9fc28d4141
ludwing979 <ludwing979@github.com>
2017-12-27 21:36:26 -0600

    lol

--- noticias.php
+++ noticias.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Publicar Noticia</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<div>
@@ -9,10 +11,17 @@
 	</div>
 	<div>
 		<p>Título de la noticia</p>
-		<input type="text" id="titulo">
+		<input type="text" id="title">
 		<p>Descripción de la noticia</p>
 		<textarea id="info"></textarea>
-		<p></p>
+		<p>Categoría</p>
+		<select>
+			<option value=""></option>
+			<option value="nuevo">Lo más Nuevo</option>
+			<option value="musicaN">Música Nueva</option>
+			<option value="NewArt">Nuevos Artistas</option>
+			<option value="dj">El dj del año</option>
+		</select>
 	</div>
 </body>
 </html>
