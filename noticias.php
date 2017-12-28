e48990846487fdc9761a4dfe7036ee2e8d6411f5
ludwing979 <ludwing979@github.com>
2017-12-27 22:44:41 -0600

    estilo noticias

--- noticias.php
+++ noticias.php
@@ -6,22 +6,25 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<div>
+	<div class="titu">
 		<h1>Publicar Noticia</h1>
 	</div>
-	<div>
-		<p>Título de la noticia</p>
-		<input type="text" id="title">
-		<p>Descripción de la noticia</p>
-		<textarea id="info"></textarea>
-		<p>Categoría</p>
-		<select>
-			<option value=""></option>
-			<option value="nuevo">Lo más Nuevo</option>
-			<option value="musicaN">Música Nueva</option>
-			<option value="NewArt">Nuevos Artistas</option>
-			<option value="dj">El dj del año</option>
-		</select>
+	<div id="fondo">
+		<div  id="formulario">
+			<p>Título de la noticia</p>
+			<input type="text" id="titleNoticia">
+			<p>Descripción de la noticia</p>
+			<textarea id="info"></textarea>
+			<p>Categoría</p>
+			<select>
+				<option value=""></option>
+				<option value="1">Lo más Nuevo</option>
+				<option value="2">Música Nueva</option>
+				<option value="3">Nuevos Artistas</option>
+				<option value="4">El dj del año</option>
+			</select>
+			<button onclick="">Publicar</button>
+		</div>
 	</div>
 </body>
 </html>
