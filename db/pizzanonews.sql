fe9ba28a3fad02bc07b1bc77e6540e3fd57dacaf
ludwing979 <ludwing979@github.com>
2018-01-05 17:11:39 -0600

    db

--- db/pizzanonews.sql
+++ db/pizzanonews.sql
@@ -1,24 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-01-2018 a las 00:10:19
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.6.21
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `pizzanonews`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
 
 CREATE TABLE `categorias` (
   `idCategoria` int(5) NOT NULL,
   `categoria` varchar(20) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
 INSERT INTO `categorias` (`idCategoria`, `categoria`) VALUES
 (1, 'Lo Más Nuevo'),
 (2, 'Música Nueva'),
 (3, 'Nuevos Artistas'),
 (4, 'El DJ del Año');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
 
 CREATE TABLE `comentarios` (
   `idComentario` int(11) NOT NULL,
@@ -28,22 +54,43 @@ CREATE TABLE `comentarios` (
   `fechaPublicacion` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
 INSERT INTO `comentarios` (`idComentario`, `idUsuario`, `idNoticia`, `contenido`, `fechaPublicacion`) VALUES
 (1, 1, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum tortor ut lacus viverra molestie. Morbi id fermentum nisi. Curabitur consectetur, odio at faucibus varius, augue augue feugiat turpis, at tincidunt orci magna ut magna. Etiam convallis nisi vitae ligula bibendum, quis pharetra metus pulvinar. Suspendisse tempor auctor accumsan. Phasellus lectus arcu, vehicula vel diam nullam.', '2017-12-28');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
 
 CREATE TABLE `noticias` (
   `idNoticia` int(11) NOT NULL,
   `titulo` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `fechaPublicacion` date NOT NULL,
   `informacion` varchar(1000) COLLATE utf8_spanish_ci NOT NULL,
-  `idCategoria` int(11) NOT NULL
+  `categoria` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-INSERT INTO `noticias` (`idNoticia`, `titulo`, `fechaPublicacion`, `informacion`, `idCategoria`) VALUES
+--
+-- Volcado de datos para la tabla `noticias`
+--
+
+INSERT INTO `noticias` (`idNoticia`, `titulo`, `fechaPublicacion`, `informacion`, `categoria`) VALUES
 (1, 'Alok, El Favorito de Brasil para el mundo!', '2017-06-01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque posuere tempus. Duis quis ipsum at nisi hendrerit tristique et.', 1),
 (2, 'Dimitri Vegas, Like Mike y Hardwell... ¡juntos de nuevo!', '2017-12-26', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget quam ac nisi aliquam rhoncus ac sed nulla. Aliquam tempus feugiat mauris, nec post.', 2),
 (3, 'Don Diablo presenta su Yearmix 2017', '2017-12-25', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget quam ac nisi aliquam rhoncus ac sed nulla. Aliquam tempus feugiat mauris, nec post.', 3),
-(4, 'El regalo de Navidad de Avicii para sus fans', '2017-12-01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tellus ex, fringilla vitae sem non, consequat rutrum nulla. Pellentesque massa nunc.', 4);
+(4, 'El regalo de Navidad de Avicii para sus fans', '2017-12-01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tellus ex, fringilla vitae sem non, consequat rutrum nulla. Pellentesque massa nunc.', 4),
+(17, 'prueba', '2018-10-14', '123', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
 
 CREATE TABLE `usuarios` (
   `idUsuario` int(11) NOT NULL,
@@ -51,35 +98,79 @@ CREATE TABLE `usuarios` (
   `contrasena` varchar(30) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
 INSERT INTO `usuarios` (`idUsuario`, `correo`, `contrasena`) VALUES
-(1, 'marcopolo_1309@hotmail.com', '123456789');
+(1, 'marcopolo_1309@hotmail.com', '123456789'),
+(2, 'yo', '123');
 
+--
+-- Índices para tablas volcadas
+--
 
+--
+-- Indices de la tabla `categorias`
+--
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`idCategoria`);
 
+--
+-- Indices de la tabla `comentarios`
+--
 ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`idComentario`),
   ADD UNIQUE KEY `idUsuario` (`idUsuario`),
   ADD UNIQUE KEY `idNoticia` (`idNoticia`);
 
+--
+-- Indices de la tabla `noticias`
+--
 ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`idNoticia`),
-  ADD UNIQUE KEY `idCate` (`idCategoria`);
+  ADD PRIMARY KEY (`idNoticia`);
 
+--
+-- Indices de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`idUsuario`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
 ALTER TABLE `categorias`
   MODIFY `idCategoria` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
 ALTER TABLE `comentarios`
   MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
 ALTER TABLE `noticias`
-  MODIFY `idNoticia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+  MODIFY `idNoticia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`idNoticia`) REFERENCES `noticias` (`idNoticia`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
